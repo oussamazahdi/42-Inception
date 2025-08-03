5fbeb62115d358507867509cd27d0f492a8c4315
oussamaza25 <oussama.zahdi25@gmail.com>
2025-08-03 12:32:37 +0100

    fixe nginx and mariadb

--- srcs/requirements/mariadb/tools/Script.sh
+++ srcs/requirements/mariadb/tools/Script.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-service mysql start;
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+
+
+#we should to add vars to secrets and .env file
+
+
+# Start MySQL server
+mysqld_safe --skip-networking &
+
+# Configure MySQL
+mysql -u root <<-EOSQL
+    CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;
+    CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';
+    GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%';
+    ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';
+    FLUSH PRIVILEGES;
+EOSQL
+
+# Stop MySQL server
+mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
+
+# Start MySQL server in safe mode
 exec mysqld_safe

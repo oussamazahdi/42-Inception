4e6d4887103f79dc02ca1ee419d368c271fa1c6c
Oussama Zahdi <oussama.zahdi25@gmail.com>
2025-08-15 05:23:45 -0400

    start bonus with redis

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -43,6 +43,18 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 		--allow-root \
 		--path=/var/www/html
 
+	wp config set WP_REDIS_HOST "${REDIS_HOST}" --allow-root
+	wp config set WP_REDIS_PORT 6379 --raw --allow-root
+	wp config set WP_CACHE_KEY_SALT "${DOMAIN_NAME}" --allow-root
+	# wp config set WP_REDIS_PASSWORD "${REDIS_PASSWORD}" --allow-root
+	wp config set WP_REDIS_CLIENT "${REDIS_USER}" --allow-root
+	
+	wp config set WP_CACHE true --raw --allow-root
+
+	wp plugin install redis-cache --activate --allow-root
+	wp plugin update redis-cache --allow-root
+	wp redis enable --allow-root
+
 	chown -R inception:inception /var/www/html
 	find /var/www/html -type d -exec chmod 755 {} \;
 	find /var/www/html -type f -exec chmod 644 {} \;

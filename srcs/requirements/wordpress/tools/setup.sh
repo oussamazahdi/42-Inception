2218a0657d3c62e98946725a45f5097b014057cb
oussama zahdi <oussama.zahdi25@gmail.com>
2025-08-27 04:43:41 -0400

    chore: update Dockerfiles and configuration for mariadb and wordpress

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-while ! nc -z mariadb 3306; do
-	sleep 1
-done
-while ! nc -z redis 6379; do
-	sleep 1
-done
+# while ! nc -z mariadb 3306; do
+# 	sleep 1
+# done
+# while ! nc -z redis 6379; do
+# 	sleep 1
+# done
 
 if [ ! -f /var/www/html/wp-config.php ]; then
 
@@ -46,18 +46,15 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	wp config set WP_REDIS_HOST "${REDIS_HOST}" --allow-root
 	wp config set WP_REDIS_PORT 6379 --raw --allow-root
 	wp config set WP_CACHE_KEY_SALT "${DOMAIN_NAME}" --allow-root
-	# wp config set WP_REDIS_PASSWORD "${REDIS_PASSWORD}" --allow-root
-	# wp config set WP_REDIS_CLIENT "${REDIS_USER}" --allow-root
-	
 	wp config set WP_CACHE true --raw --allow-root
 
 	wp plugin install redis-cache --activate --allow-root
 	wp plugin update redis-cache --allow-root
 	wp redis enable --allow-root
 
-	chown -R inception:inception /var/www/html
-	find /var/www/html -type d -exec chmod 755 {} \;
-	find /var/www/html -type f -exec chmod 644 {} \;
+	# chown -R www-data:www-data /var/www/html
+	# find /var/www/html -type d -exec chmod 755 {} \;
+	# find /var/www/html -type f -exec chmod 644 {} \;
 fi
 
 exec php-fpm7.4 -F
